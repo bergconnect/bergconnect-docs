40ad589b54fa141461a4a15edbfdd8ad347ad51a
lvdberg <leon@berg-connect.nl>
2025-09-06 01:06:56 +0200

    customize

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,7 +85,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/berg-connect-faded-edges.png',
       navbar: {
         title: 'docs.berg-connect.nl',
         logo: {
