83feec53ea19c8bbf4c711d869d91a574bcfd2f0
lvdberg <leon@berg-connect.nl>
2025-09-08 22:48:06 +0200

    update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -150,7 +150,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} berg-connect.`,
       },
       prism: {
         theme: prismThemes.github,
